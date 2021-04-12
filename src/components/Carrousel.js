7437da98246df480259395fe465fdc49ae017406
micho19 <damiangiannico@gmail.com>
2021-04-12 13:01:52 -0300

    slider andando

--- src/components/Carrousel.js
+++ src/components/Carrousel.js
@@ -1,101 +1,25 @@
 import React from 'react';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-
 import {
   SliderBackDiv,
   CarrouselSlider,
-  SliderDiv,
   TitleDiv,
   TitleH1,
-  EmployerDiv,
-  ImgBelen,
-  Employerh3,
-  Employerh4,
-  EmployerP,
-  EmployerSpan,
 } from '../styles/componentStyles/CarrouselStyle.js';
 
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/swiper-bundle';
-import belen from '../assets/belen.jfif';
-import facundo from '../assets/facundo.jfif';
 
-// const swiper = new Swiper('.swiper-container', {
-//   speed: 400,
-//   spaceBetween: 100,
-// });
-// var slideIndex = 0;
-function showSlides() {
-  let slides = document.getElementsByClassName('mySlides');
-  let arraySlides = [...slides];
-  console.log(arraySlides);
-  // setInterval(() => {
-  //   console.log(Swiper.activeIndex);
-  // }, 2000);
-  // setTimeout(showSlides, 2000);
-}
+import SwiperSlider from './SwiperSlider.js';
+
 const Carrousel = () => {
-  showSlides();
-  // let swiper = new Swiper('.swiper-container', {
-  //   navigation: {
-  //     nextEl: '.swiper-button-next',
-  //     prevEl: '.swiper-button-prev',
-  //   },
-  // });
-
-  // Now you can use all slider methods like
-  // swiper.slideNext();
-
-  // const slidechangefn = () => {
-  //   setInterval(() => {
-  //     Swiper.SlideChange();
-  //   }, 2000);
-  // };
   return (
     <SliderBackDiv>
       <CarrouselSlider>
         <TitleDiv>
           <TitleH1>NUESTRO EQUIPO</TitleH1>
         </TitleDiv>
-
-        <Swiper
-          className="swiper-container"
-          spaceBetween={50}
-          slidesPerView={1}
-          navigation
-          autoplay
-          pagination={{ clickable: true }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log('slide change')}
-        >
-          <SwiperSlide className="mySlides">
-            <SliderDiv>
-              <EmployerDiv>
-                <ImgBelen src={belen} alt="" />
-                <Employerh3>María Belén Arroyo</Employerh3>
-                <Employerh4>Organizadora de eventos</Employerh4>
-                <EmployerP>
-                  Soy curiosa e inquieta. Mi talento es el francés
-                </EmployerP>
-                <EmployerSpan>"Crea tu destino"</EmployerSpan>
-              </EmployerDiv>
-            </SliderDiv>
-          </SwiperSlide>
-          <SwiperSlide className="mySlides">
-            <SliderDiv>
-              <EmployerDiv>
-                <ImgBelen src={facundo} alt="" />
-                <Employerh3>Facundo Corbalan</Employerh3>
-                <Employerh4>
-                  Experto en Payroll y procesos de implementación
-                </Employerh4>
-                <EmployerP>Apasionado por su profesión</EmployerP>
-                <EmployerSpan>"De cara al futuro siempre"</EmployerSpan>
-              </EmployerDiv>
-            </SliderDiv>
-          </SwiperSlide>
-        </Swiper>
+        <SwiperSlider />
       </CarrouselSlider>
     </SliderBackDiv>
   );
--- src/components/SwiperSlider.js
+++ src/components/SwiperSlider.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+import {
+  SliderDiv,
+  EmployerDiv,
+  ImgBelen,
+  Employerh3,
+  Employerh4,
+  EmployerP,
+  EmployerSpan,
+} from '../styles/componentStyles/CarrouselStyle.js';
+import belen from '../assets/belen.jfif';
+import facundo from '../assets/facundo.jfif';
 
 const SwiperSlider = () => {
+  SwiperCore.use([Autoplay]);
+
   return (
     <div>
-      <div></div>
+      <Swiper
+        className="swiper-container"
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        autoplay={{ delay: 6000 }}
+        pagination={{ clickable: true }}
+
+        // onSwiper={(swiper) => console.log(swiper)}
+        // onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide className="mySlides">
+          <SliderDiv>
+            <EmployerDiv>
+              <ImgBelen src={belen} alt="" />
+              <Employerh3>María Belén Arroyo</Employerh3>
+              <Employerh4>Fundadora de Ensemble HR</Employerh4>
+              <EmployerP>Futura Licenciada en Relaciones del Trabajo</EmployerP>
+              <EmployerSpan>"Crea tu destino"</EmployerSpan>
+            </EmployerDiv>
+          </SliderDiv>
+        </SwiperSlide>
+        <SwiperSlide className="mySlides">
+          <SliderDiv>
+            <EmployerDiv>
+              <ImgBelen src={facundo} alt="" />
+              <Employerh3>Facundo Corbalan</Employerh3>
+              <Employerh4>
+                Experto en Payroll y procesos de implementación
+              </Employerh4>
+              <EmployerP>Apasionado por su profesión</EmployerP>
+              <EmployerSpan>"De cara al futuro siempre"</EmployerSpan>
+            </EmployerDiv>
+          </SliderDiv>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
